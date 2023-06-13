04a5f317a31462f0dd384fb8841fccfab71a75a5
mariankatysovska <133611488+mariankatysovska@users.noreply.github.com>
2023-06-13 17:27:05 -0400

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,67 +1,83 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
+
 
 public class BaseTest {
     public static WebDriver driver = null;
 
     public static String url = "https://qa.koel.app/";
     public static WebDriverWait wait = null;
-    public static Actions actions = null;
-
 
 
     @BeforeSuite
     static void setupDriver() {
         WebDriverManager.chromedriver().setup();
-
     }
-    @BeforeMethod
-    public void setUpBrowser(){
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--remote-allow-origins=*");
-//        options.addArguments("--disable-notification*");
-//        options.addArguments("--start-maximized");
 
-        driver = pickBrowser(System.getProperty("browser"));
+    @BeforeMethod
+//    @Parameters({"BaseURL"})
+    //  public void setUpBrowser(String BaseURL){
+    public void setUpBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         openUrl(url);
-
     }
 
-    public WebDriver pickBrowser(String browser) {
-        switch (browser){
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.200.3:5555";
+        switch (browser) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 return driver = new FirefoxDriver();
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "grid-firefox":
+                capabilities.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-safari":
+                capabilities.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-edge":
+                capabilities.setCapability("browserName", "edge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-chrome":
+                capabilities.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
             default:
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
-                options.addArguments("--disable-notification*");
+                options.addArguments("--disable-notifications");
                 options.addArguments("--start-maximized");
-
                 return driver = new ChromeDriver(options);
-
         }
     }
+
 
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
@@ -71,33 +87,5 @@ public class BaseTest {
     public void openUrl(String url) {
         driver.get(url);
     }
-    public void enterEmail(String email) {
-        // WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
-        WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys(email);
-    }
-    public void enterPassword(String password) {
-        // WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys(password);
 
     }
-    public void clickLoginBtn() {
-        //  WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
-        WebElement submitLogin = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
-        submitLogin.click();
-        // WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
-        WebElement avatar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".avatar")));
-        Assert.assertTrue(avatar.isDisplayed());
-    }
-    public void login(String email, String password){
-        enterEmail(email);
-        enterPassword(password);
-        clickLoginBtn();
-    }
-
-}
