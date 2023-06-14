db1d734ab9a04e178497e99211136f3d0f275cb9
mariankatysovska <133611488+mariankatysovska@users.noreply.github.com>
2023-06-14 18:23:13 -0400

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,3 @@
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,13 +10,13 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.time.Duration;
-import java.util.Locale;
+
 
 public class BaseTest {
     private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
@@ -26,14 +25,13 @@ public class BaseTest {
     public static WebDriverWait wait = null;
     public static WebDriver getThreadLocal() {
         return THREAD_LOCAL.get();
-<<<<<<< Updated upstream
-=======
+
     }
 
     @BeforeSuite
     static void setupDriver() {
         WebDriverManager.chromedriver().setup();
->>>>>>> Stashed changes
+
     }
 
 
@@ -45,7 +43,7 @@ public class BaseTest {
         System.out.println(
                 "Browser setup by Thread " + Thread.currentThread().getId() + " and Driver reference is : " + getThreadLocal());
 
-<<<<<<< Updated upstream
+
     }
 
     @AfterMethod(alwaysRun = true)
@@ -54,43 +52,6 @@ public class BaseTest {
         THREAD_LOCAL.remove();
     }
 
-    private WebDriver pickBrowser(String browser) throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        String gridURL = "http://192.168.0.159:4444";
-        switch (browser){
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                return driver = new FirefoxDriver();
-            case "safari":
-                WebDriverManager.safaridriver().setup();
-                return driver = new SafariDriver();
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                return driver = new EdgeDriver();
-            case "grid-firefox":
-                capabilities.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-            case "grid-safari":
-                capabilities.setCapability("browserName", "safari");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-            case "cloud":
-                return lambdaTest();
-            default:
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--remote-allow-origins=*");
-                options.addArguments("--disable-notifications");
-                options.addArguments("--start-maximized");
-               // options.addArguments("--headless=new");
-                return driver = new ChromeDriver(options);
-        }
-=======
-    }
-    @AfterMethod(alwaysRun = true)
-    public void tearDown(){
-        THREAD_LOCAL.get().close();
-        THREAD_LOCAL.remove();
-    }
     public WebDriver pickBrowser(String browser) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         String gridURL = "http://192.168.200.3:5555";
@@ -146,40 +107,6 @@ public class BaseTest {
 
     public void openUrl(String url) {
         driver.get(url);
->>>>>>> Stashed changes
+
     }
-
-
-    public WebDriver lambdaTest() throws MalformedURLException {
-        String username = "smisl.zhizni";
-        String authkey = "l4unZCXif463zVpLgdW8JI7C1KOX1lOa2ItC1iUllLYfiT6OMi";
-        String hub = "@hub.lambdatest.com/wd/hub";
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platform", "Windows 10");
-        caps.setCapability("browserName", "Firefox");
-        caps.setCapability("version", "112.0");
-        caps.setCapability("resolution", "1024x768");
-        caps.setCapability("build", "TestNG With Java");
-        caps.setCapability("name", this.getClass().getName());
-        caps.setCapability("plugin", "git-testng");
-        return new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
-    }
-<<<<<<< Updated upstream
-
-
-    @DataProvider(name="IncorrectLoginProviders")
-    public static Object[][] getDataFromDataProviders(){
-        return new Object[][]{
-                {"notExisting@email.com", "NotExistingPassword"},
-                {"demo@class.com", ""},
-                {"", ""},
-        };
-    }
-
-
-
 }
-=======
-
-
->>>>>>> Stashed changes
